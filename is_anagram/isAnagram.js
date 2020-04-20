ed04661d0518636f171089886bcaaffbd075afa4
Thiago Oliveira <thiago@signaes.com>
2020-04-20 15:01:39 -0300

    Inital commit

---
+++ is_anagram/isAnagram.js
@@ -0,0 +1,33 @@
+function isAnagram(a, b) {
+  if (a.length != b.length) {
+    return false;
+  }
+
+  const aMap = {};
+  const bMap = {};
+
+  for (let c of a) {
+    aMap[c] = !!aMap[c] + 1;
+  }
+
+  for (let c of b) {
+    bMap[c] = !!bMap[c] + 1;
+  }
+
+  for (let c of a) {
+    if (aMap[c] !== bMap[c]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(`isAnagram('', '') ${isAnagram('', '')}`);
+console.log(`isAnagram('aaz', 'zza') ${isAnagram('aaz', 'zza')}`);
+console.log(`isAnagram('anagram', 'nagaram') ${isAnagram('anagram', 'nagaram')}`);
+console.log(`isAnagram('rat', 'car') ${isAnagram('rat', 'car')}`);
+console.log(`isAnagram('awesome', 'awesom') ${isAnagram('awesome', 'awesom')}`);
+console.log(`isAnagram('amanaplanacanalpanama', 'acanalmanplanpamana') ${isAnagram('amanaplanacanalpanama', 'acanalmanplanpamana')}`);
+console.log(`isAnagram('qwerty', 'qeywrt') ${isAnagram('qwerty', 'qeywrt')}`);
+console.log(`isAnagram('texttwisttime', 'timetwisttext') ${isAnagram('texttwisttime', 'timetwisttext')}`);
